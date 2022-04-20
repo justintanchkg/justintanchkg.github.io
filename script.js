e64d405f403041ad50d2e9bd75c6cb3d7fe4ebbc
justintanchkg <justintanchkg@gmail.com>
2022-04-20 10:31:19 +0800

    inputting array for custom attribute functionality

--- script.js
+++ script.js
@@ -139,7 +139,10 @@ document.getElementById("phone_number_button").addEventListener("click", functio
 document.getElementById("attribute_button").addEventListener("click", function(){
   attributeName = document.getElementById("attribute_name").value;
   attributeValue = document.getElementById("attribute_value").value;
-  dataLayer.push({'brazeAttributeName': attributeName});
+  dataLayer.push({
+    'brazeAttributeName': attributeName,
+    'brazeAttributeValue': attributeValue,
+  });
   console.log("attribute logging attempted");
   // TODO ADD CODE HERE
 
